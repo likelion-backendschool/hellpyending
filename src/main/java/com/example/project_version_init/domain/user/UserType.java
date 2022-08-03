542977d0ca154e6ac4b1a46a4ce5f73b8226233d
james-dev <chungjm0711@gmail.com>
2022-08-03 16:44:54 +0900

    [Feat] : 유저 entity 생성

---
+++ src/main/java/com/example/project_version_init/domain/user/UserType.java
@@ -0,0 +1,17 @@
+package com.example.project_version_init.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserType {
+    ADMIN("ROLE_ADMIN", "administrator"),
+    USER_1("ROLE_USER_LEVEL1", "user"),
+    USER_2("ROLE_USER_LEVEL2", "user"),
+    USER_3("ROLE_USER_LEVEL3", "user"),
+    ;
+
+    private final String key;
+    private final String title;
+}
