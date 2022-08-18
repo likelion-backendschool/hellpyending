df8819c0064e7b868cb0e66d778cbbe06a9c47ef
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-18 17:07:15 +0900

    #33 - 정적 리소스 security 대상에서 제외

--- src/main/java/com/example/hellpyending/config/SpringSecurityConfig.java
+++ src/main/java/com/example/hellpyending/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.hellpyending.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +20,12 @@ public class SpringSecurityConfig {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll();
         return http.build();
+    }
+    // web.ignoring().antMatchers("/images/**", "/css/**"); // 아래 코드와 같은 코드입니다.
+    // 정적 리소스 spring security 대상에서 제외
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());;
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
