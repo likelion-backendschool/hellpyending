1853b707d085a3d5ba7381c9df0ac8666ca288e1
ghdtmdvy2 <73926388+ghdtmdvy2@users.noreply.github.com>
2022-08-18 16:29:43 +0900

    #38 - 에러 수정 (#39)

--- src/main/java/com/example/hellpyending/article/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hellpyending.article.repository;
-
-import com.example.hellpyending.article.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
