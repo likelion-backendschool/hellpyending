f878bf96411baf67fea3d0840c8a519e0aa25993
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-22 09:22:13 +0900

    #33 - 임시유저를 사용하기 위한 application.yaml 파일에 hibernate.ddl-auto를 update에서 create로 바꿈

--- src/main/java/com/example/hellpyending/config/InitUserDefaultConfig.java
+++ src/main/java/com/example/hellpyending/config/InitUserDefaultConfig.java
@@ -18,38 +18,38 @@ public class InitUserDefaultConfig {
     private final PasswordEncoder passwordEncoder;
     @Bean
     public void initializeDefaultAdmin() {
-//        Users user1 = Users.builder()
-//                .username("admin")
-//                .password(passwordEncoder.encode("admin"))
-//                .sex(Sex.MALE)
-//                .email("admin@admin.com")
-//                .phoneNumber("01012341234")
-//                .deleteYn('N')
-//                .birthday(LocalDate.now())
-//                .address_1st("어드민")
-//                .address_2st("어드민")
-//                .address_3st("어드민")
-//                .address_4st("어드민")
-//                .address_detail("어드민")
-//                .userType(UserType.ADMIN)
-//                .build();
-//        userService.create(user1);
-//
-//        Users user2 = Users.builder()
-//                .username("user")
-//                .password(passwordEncoder.encode("user"))
-//                .sex(Sex.MALE)
-//                .email("user@user.com")
-//                .phoneNumber("01011111111")
-//                .deleteYn('N')
-//                .birthday(LocalDate.now())
-//                .address_1st("유저")
-//                .address_2st("유저")
-//                .address_3st("유저")
-//                .address_4st("유저")
-//                .address_detail("유저")
-//                .userType(UserType.USER)
-//                .build();
-//        userService.create(user2);
+        Users user1 = Users.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .sex(Sex.MALE)
+                .email("admin@admin.com")
+                .phoneNumber("01012341234")
+                .deleteYn('N')
+                .birthday(LocalDate.now())
+                .address_1st("어드민")
+                .address_2st("어드민")
+                .address_3st("어드민")
+                .address_4st("어드민")
+                .address_detail("어드민")
+                .userType(UserType.ADMIN)
+                .build();
+        userService.create(user1);
+
+        Users user2 = Users.builder()
+                .username("user")
+                .password(passwordEncoder.encode("user"))
+                .sex(Sex.MALE)
+                .email("user@user.com")
+                .phoneNumber("01011111111")
+                .deleteYn('N')
+                .birthday(LocalDate.now())
+                .address_1st("유저")
+                .address_2st("유저")
+                .address_3st("유저")
+                .address_4st("유저")
+                .address_detail("유저")
+                .userType(UserType.USER)
+                .build();
+        userService.create(user2);
     }
 }
