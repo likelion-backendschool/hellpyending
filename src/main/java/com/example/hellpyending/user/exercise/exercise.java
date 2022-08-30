3e265ed132445571791b4341446bd838bc69091a
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-30 16:51:24 +0900

    #65 - 운동 엔티티 추가

--- src/main/java/com/example/hellpyending/user/exercise/exercise.java
+++ src/main/java/com/example/hellpyending/user/exercise/exercise.java
@@ -1,5 +1,32 @@
 package com.example.hellpyending.user.exercise;
 
-public class exercise {
+import com.example.hellpyending.config.BaseTimeEntity;
+import com.example.hellpyending.user.entity.Users;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class exercise extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200) // varchar(200)
+    private String Type;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private ExerciseIntensity Intensity;
+
+    @Column
+    private Integer hour;
+
+    @Column
+    private Integer calorie;
+
+    @ManyToOne
+    private Users author;
 
 }
