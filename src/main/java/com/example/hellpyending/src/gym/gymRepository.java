026ef01283b422bc608f0d0a944fc634940df837
JI-NAMEUN <skadms1720@github.com>
2022-09-05 16:51:47 +0900

    헬스장 별로 상세페이지 만들기

--- src/main/java/com/example/hellpyending/src/gym/gymRepository.java
+++ src/main/java/com/example/hellpyending/src/gym/gymRepository.java
@@ -1,5 +1,6 @@
 package com.example.hellpyending.src.gym;
 
+import com.example.hellpyending.src.gym.entity.GetAddressRes;
 import com.example.hellpyending.src.gym.entity.GetAddressResInterface;
 import com.example.hellpyending.src.gym.entity.Gym;
 import com.example.hellpyending.user.entity.Users;
@@ -23,4 +24,6 @@ public interface gymRepository extends JpaRepository<Gym, Integer> {
     List<GetAddressResInterface> findByGymList_2st(String address_1st, String address_2st);
     @Query(nativeQuery = true,value = "select g.gym_name, g.lat,g.lng,g.gym_address,g.gym_phone_number,g.per1_month,g.per3_months,g.per6_months,g.per12_months from gym as g where address_1st=:address_1st and address_2st=:address_2st and address_3st=:address_3st")
     List<GetAddressResInterface> findByGymList_3st( String address_1st, String address_2st,String address_3st);
+    @Query(nativeQuery = true,value = "SELECT g.gym_name, g.lat,g.lng,g.gym_address,g.gym_phone_number,g.per1_month,g.per3_months,g.per6_months,g.per12_months  FROM gym as g WHERE g.lat = ?1 AND g.lng = ?2")
+    GetAddressResInterface findByLatAndLng(double lat, double lng);
 }
