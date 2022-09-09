baebaa580b39032f2b84be45ae9d30ea362fdeef
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-09 19:53:03 +0900

    #95 - 토근을 발행 하는 맵핑 추가

--- src/main/java/com/example/hellpyending/controller/HelloController.java
+++ src/main/java/com/example/hellpyending/controller/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.hellpyending.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/")
+    public OAuth2AuthenticationToken home(final  OAuth2AuthenticationToken token){
+        return token;
     }
 }
