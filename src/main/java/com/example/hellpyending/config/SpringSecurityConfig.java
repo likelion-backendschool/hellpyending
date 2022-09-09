be4052a0c4cbe0b97a02251f48becf503cea51d8
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-09 20:21:29 +0900

    #95 - 커스텀 마이징을 통한 엑세스 권한 추가

--- src/main/java/com/example/hellpyending/config/SpringSecurityConfig.java
+++ src/main/java/com/example/hellpyending/config/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.hellpyending.config;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
@@ -12,16 +14,27 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity(debug = true) // WebSecurity에서 어떤 필터를 거쳤는지 알 수 있음.
+@AllArgsConstructor
 public class SpringSecurityConfig {
-
+    private final Environment environment; // 각 환경에 대한 설정 변경이 필요하기 위해 가져옴.
+    private final String registration = "spring.security.oauth2.client.registration."; // facebook과 google의 커스텀 마이징을 위한 yml 파일 가져오기.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
@@ -58,7 +71,9 @@ public class SpringSecurityConfig {
                 .permitAll();
         // oauth2 login
         http.oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/")
+                        .clientRegistrationRepository(clientRegistrationRepository())
+                        .authorizedClientService(auth2AuthorizedClientService())
+                        .defaultSuccessUrl("/",false)
                 );
                 // login 성공시 루트 페이지로 이동 ( alwayUse를 false로 입력 시 접속 하려던 URL로 바로 이동 )
 
@@ -94,6 +109,61 @@ public class SpringSecurityConfig {
         // ADMIN 권한은 USER 권한을 가진다. ( ADMIN 권한은 USER 권한보다 더 세다(?) )
         roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
         return roleHierarchy;
+    }
+
+    /*
+    ClientRegistration에 대한 정보는 Authorization Server에서 해당 클라이언트의 정보가 필요할 때 사용한다.
+    -> 즉 접속할 Provider에 대한 정보를 구현하는 것이다.
+    */
+    private ClientRegistration googleClientRegistration(){
+        // yml 파일에서 spring.security.oauth2.client.registration.google.client-id 내용을 가져옴.
+        final String clientId = environment.getProperty(registration + "google.client-id");
+        // yml 파일에서 spring.security.oauth2.client.registration.google.client-secret 내용을 가져옴.
+        final String clientSecret = environment.getProperty(registration + "google.client-secret");
+
+        return CommonOAuth2Provider
+                .GOOGLE
+                .getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+
+    private ClientRegistration facebookClientRegistration(){
+        // yml 파일에서 spring.security.oauth2.client.registration.facebook.client-id 내용을 가져옴.
+        final String clientId = environment.getProperty(registration + "facebook.client-id");
+        // yml 파일에서 spring.security.oauth2.client.registration.facebook.client-secret 내용을 가져옴.
+        final String clientSecret = environment.getProperty(registration + "facebook.client-secret");
+
+        return CommonOAuth2Provider
+                .FACEBOOK
+                .getBuilder("facebook")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .scope(
+                        "public_profile",
+                        "email", // 이메일 수집
+                        "user_birthday", // 생년월일 수집
+                        "user_gender" // 성별 수집
+                )
+                .userInfoUri("https://graph.facebook.com/me?fields=id,name,email,gender,birthday")
+                .build();
+    }
+    // 실제 토큰과 OAuth 와 통신
+    @Bean
+    public OAuth2AuthorizedClientService auth2AuthorizedClientService(){
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(){
+        // 각 facebook, google의 provider에 대한 정보를 list로 저장.
+        final List<ClientRegistration> clientRegistrations = Arrays.asList(
+                googleClientRegistration(),
+                facebookClientRegistration()
+        );
+        // 각 provider를 ClientRegistrationRepository 에다가 저장.
+        // ClientRegistrationRepository : ClientRegistration에 대한 정보를 저장하는 저장소.
+        return new InMemoryClientRegistrationRepository(clientRegistrations);
     }
 
 
