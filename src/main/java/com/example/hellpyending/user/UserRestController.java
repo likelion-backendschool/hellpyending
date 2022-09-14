bd26b9d015528ff1ef54203e33e86f475e1a2979
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-15 08:54:52 +0900

    #95 - .gitignore 수정

--- src/main/java/com/example/hellpyending/user/UserRestController.java
+++ src/main/java/com/example/hellpyending/user/UserRestController.java
@@ -2,14 +2,16 @@ package com.example.hellpyending.user;
 
 import com.example.hellpyending.user.entity.Users;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.security.Principal;
 import java.util.Optional;
 
@@ -18,6 +20,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserRestController {
     private final UserService userService;
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}") // 보내는 사람 메일 주소
+    private String from;
     @GetMapping("/information/check")
     @ResponseBody
     public String check(Principal principal, String nickname){
@@ -27,5 +33,12 @@ public class UserRestController {
         }
 
         return "닉네임이 중복 되었습니다.";
+    }
+    @GetMapping("/password/find")
+    public String pwdFind() throws MessagingException {
+        MimeMessage mimeMailMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMailMessage,true,"UTF-8");
+        mimeMessageHelper.setFrom("ghdtmdvy2@naver.com");
+        mimeMessageHelper.setTo("dhfl0909@naver.com");
     }
 }
